6c9608ef5b813ff6903a655218879d97eb3b406a
larissapassos <lnpa@cin.ufpe.br>
2015-02-14 16:25:51 -0500

    Streamlining

--- app.js
+++ app.js
@@ -12,3 +12,7 @@ var server = app.listen(3000, function () {
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
+app.get('/news', function (req, res) {
+    res.send('olar.mundo');
+});
