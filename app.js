80acdd67842aeee6a06f104c0d3912b8ee5b2b37
larissapassos <lnpa@cin.ufpe.br>
2015-02-14 16:52:00 -0500

    Rendering the Template

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var express = require('express');
 var morgan = require('morgan');
+var swig = require('swig');
 
 var app = express();
 
+// logging
 app.use(morgan('dev'));
+
+// Integration to swig
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// Disabling swig caching
+swig.setDefaults({ cache: false });
+
 
 var server = app.listen(3000, function () {
     console.log('server listening');
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render('index', {title: 'Hall of Fame', people: people});
 });
 
 app.get('/news', function (req, res) {
-    res.send('olar.larigirl');
+    res.send('olar.mundo');
 });
