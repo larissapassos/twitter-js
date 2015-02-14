2259299a8d6a91366826502ae1372d250d59f507
larissapassos <lnpa@cin.ufpe.br>
2015-02-14 16:09:49 -0500

    Starting a server

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+
+var server = app.listen(3000, function () {
+    console.log('server listening');
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
