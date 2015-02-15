153db9bbfb9f20c2422b2e6c0fa9f9624ad13acc
Beatriz Santana Fagundes Souza de Lima <bs2899@columbia.edu>
2015-02-14 19:21:27 -0500

    Dynamically Pre-Populating Fields

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var express = require('express');
 var morgan = require('morgan');
 var swig = require('swig');
 var routes = require('./routes/');
+var bodyparser = require('body-parser');
 
 var app = express();
 
 // logging
 app.use(morgan('dev'));
 
+// Parsing URL and JSON
+app.use(bodyparser.urlencoded( {extended: false} ));
+app.use(bodyparser.json());
+
 // Using the routes folder
 app.use('/', routes);
 app.use(express.static(__dirname + '/public'));
+
 
 // Integration to swig
 app.engine('html', swig.renderFile);
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,27 @@ var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
     var tweets = tweetBank.list();
-    res.render('index', { title: 'Twitter.js', tweets: tweets });
+    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true, user: 'Nimit Maru' });
 });
 
 router.get('/users/:name', function (req, res) {
     var name = req.params.name;
     var list = tweetBank.find( {name: name} );
-    res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list });
+    res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list, showForm: true, user: name});
 });
 
 router.get('/users/:name/tweets/:id', function (req, res) {
     var name = req.params.name;
     var id = Number(req.params.id);
     var tweet = tweetBank.find( {id: id, name: name} );
-    res.render( 'index', { title: 'Tweet by ' + name, tweets: tweet});
+    res.render( 'index', { title: 'Tweet by ' + name, tweets: tweet, showForm: false, user: name});
+});
+
+router.post('/submit', function(req, res) {
+	var name = req.body.name;
+  	var text = req.body.text;
+  	tweetBank.add(name, text);
+  	res.redirect('/');
 });
 
 
